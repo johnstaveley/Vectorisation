4f9f29634d9c652f130848bc9bd9071e747056a2
johnstaveley <john@staveley.org>
2026-02-07 05:22:21 +0000

    Fix package issues

--- AspireApp1.AppHost/AppHost.cs
+++ AspireApp1.AppHost/AppHost.cs
@@ -1,7 +1,10 @@
+using Aspire.Hosting;
+using CommunityToolkit.Aspire.Hosting.Ollama;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var ollama = builder.AddOllama("ollama");
 
-var opensearch = builder.AddOpenSearch("opensearch");
+var opensearch = builder.AddElasticsearch("elasticsearch");
 
 builder.Build().Run();
